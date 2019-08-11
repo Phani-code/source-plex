8a1f0572ea375753799f48d874d3290d144df1d0
Phani-code <54002749+Phani-code@users.noreply.github.com>
2019-08-11 23:54:13 +0530

    eligibility code created

---
+++ eligibilitytest.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+
+   int a;
+   printf("enter your age : ");
+   scanf("%d",&a);
+   
+   if(a>=18)
+   printf("you are eligible for playing this game");
+   
+   else
+   printf("you are not eligible for playing this game");
